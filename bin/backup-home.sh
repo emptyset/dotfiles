1af7e95fb172d4da5735bff0e0bc268ad07f85fb
alanfay <alan.fay@sendgrid.com>
2017-12-18 08:09:45 -0700

    backup script

--- bin/backup-home.sh
+++ bin/backup-home.sh
@@ -5,5 +5,14 @@ FILENAME=${TODAY}_HOME.tar.bz2
 
 pushd ~
 # TODO: specify list of directories to exclude in private .*env files
-tar --exclude bak/ --exclude Library/ --exclude tmp/ -cpjvf bak/$FILENAME . 
+tar \
+    --exclude .dotfiles/chef/ \
+    --exclude .dotfiles/docker/ \
+    --exclude .dotfiles/vagrant/ \
+    --exclude .dotfiles/ \
+    --exclude bak/ \
+    --exclude Library/ \
+    --exclude tmp/ \
+    --exclude="VirtualBox VMs/" \
+    -cpjvf bak/$FILENAME .
 popd
